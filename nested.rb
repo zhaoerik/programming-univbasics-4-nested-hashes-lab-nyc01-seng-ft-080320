252afc5bff1b502be674ccaaa612a55e4eb7c6f3
Erik Zhao <erikzhao98@gmail.com>
2020-07-23 01:19:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,8 +80,8 @@ def adding_matz
         :languages => "C"
       }
     }
-programmer_hash[:yukihiro_matsumoto] = {
-	:known_for => "Ruby",>
+programmer_hash[:yukihiro_matsumoto] => {
+	:known_for => "Ruby",
 	:langauges => ["LISP", "C"]
 }
 end

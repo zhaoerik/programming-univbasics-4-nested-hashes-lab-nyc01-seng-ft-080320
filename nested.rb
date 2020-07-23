359675e33ab2d4a40de49e3e4583616f3f68a1a0
Erik Zhao <erikzhao98@gmail.com>
2020-07-23 01:18:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,7 +124,7 @@ def adding_to_dennis
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => "C"
       }
     }
 programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
